1d231616ae292c5984be4685129a7a75e405ac79
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-03-16 08:48:02 +0000

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import * as math from 'mathjs';
+
 
 function App() {
+  
+  const [displayValue, setDisplayValue] = useState("")
+  const [input, setInput] =   useState("")
+  const [result, setResult] = useState(0)
+
+  function handleMath(event){
+      setInput(event.target.value)
+  }
+
+  function handleButtonClick(value) {
+    if (value === "AC") {
+      setDisplayValue("");
+      setResult(0);
+    } else if (value === "=") {
+      try {
+        const result = math.evaluate(displayValue);
+        setResult(result.toString());
+        setDisplayValue("");
+      } catch {
+        setResult("Error");
+        setDisplayValue("");
+      }
+
+    }else {
+        if(value === "X"){
+          setDisplayValue(displayValue + "*");
+      }else {
+          setDisplayValue(displayValue + value)
+      }
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className='calculator'>
+         
+      <div className='formularScreen' >
+          <input value={displayValue} onChange={handleMath}/>
+      </div>
+      <div className='outPutScreen' >
+        <p>{result}</p>
+      </div>
+      <div>
+          
+          <Numbers value="AC" handleButtonClick={handleButtonClick}  />
+          <Numbers value="/"  handleButtonClick={handleButtonClick} />
+          <Numbers value="X"  handleButtonClick={handleButtonClick}  />
+          <Numbers value="7"  handleButtonClick={handleButtonClick} />
+          <Numbers value="8"  handleButtonClick={handleButtonClick} />
+          <Numbers value="9" className="square" handleButtonClick={handleButtonClick} />
+          <Numbers value="-" className="square" handleButtonClick={handleButtonClick} />
+          <Numbers value="4" className="square" handleButtonClick={handleButtonClick} />
+          <Numbers value="5" className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="6" className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="+" className="square" handleButtonClick={handleButtonClick}/>
+          <Numbers value="1" className="square" handleButtonClick={handleButtonClick} />
+          <Numbers value="2" className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="3" className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="0" className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="." className="square" handleButtonClick={handleButtonClick}  />
+          <Numbers value="=" className="square" handleButtonClick={handleButtonClick}  />
+      </div>
+     
     </div>
+    <div className='author'>
+          Design and Coded by <br />
+          <a href='https://www.freecodecamp.org/aliveevie' >Ibrahim Abdulkarim</a>
+          
+      </div>
+  </>
   );
+}
+
+function Numbers({value, handleButtonClick}){
+  
+ 
+
+  function handleClick(){
+   
+      handleButtonClick(value)
+    
+  }
+  
+  return (
+    <button onClick={handleClick}>{value}</button>
+  )
 }
 
 export default App;
